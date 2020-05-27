febfd1685c93e8f9f13f152b50f48ee5bce133b1
Denis Chepkasov <denis@chepkasov.dev>
2020-05-27 16:54:32 +0200

    Changed sort() call

--- functions/word-count.js
+++ functions/word-count.js
@@ -1,14 +1,14 @@
 const inputString = 'olly olly in come free';
 
 const wordCounter = (input) => {
-  const words = input.split(' ').sort();
+  const words = input.split(' ');
 
   const wordsObj = words.reduce((acc, curr) => {
     acc[curr] = acc[curr] ? ++acc[curr] : 1;
     return acc;
   }, {});
 
-  for (let [key, value] of Object.entries(wordsObj)) {
+  for (let [key, value] of Object.entries(wordsObj).sort()) {
     console.log((`${key}: ${value}`));
   }
 };
