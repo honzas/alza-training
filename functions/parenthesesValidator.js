e5b3f66d91b52989e942a80978d5c23cea178f9d
Denis Chepkasov <denis@chepkasov.dev>
2020-05-28 13:53:56 +0200

    Edited parenthesesValidator - matching only brackets

--- functions/parenthesesValidator.js
+++ functions/parenthesesValidator.js
@@ -2,17 +2,21 @@ const inputCorrect = '(()())';
 const inputIncorrect = '(()';
 
 const parenthesesValidator = (input) => {
+  const regex = /(\(|\))/g;
+
+  const parens = input.match(regex);
+
   let map = {
     ")": "("
   }
 
   let stack = [];
 
-  for (let item = 0; item < input.length; item++) {
-    if (input[item] === "(") {
-      stack.push(input[item]);
+  for (let item = 0; item < parens.length; item++) {
+    if (parens[item] === "(") {
+      stack.push(parens[item]);
     } else {
-      if (stack[stack.length - 1] === map[input[item]]) {
+      if (stack[stack.length - 1] === map[parens[item]]) {
         stack.pop();
       } else return `Result: ${false}`;
     }
